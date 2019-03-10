f46ca47f9280b77c3f6939fb2da1f5edfffb70a2
benjo guevarra <Benjo@BenjoPC>
2019-03-10 22:20:49 +0800

    initial

---
+++ PracticeConsole/PracticeConsole/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // call main menu
+            MainMenu();
+        }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("===============================");
+            Console.WriteLine("1. Login 2. Register 0. Exit");
+            Console.WriteLine("===============================");
+            string x = Console.ReadLine();
+
+            if (x.StartsWith("1"))
+            {
+                Login();
+            }
+            else if (x.StartsWith("2"))
+            {
+                Register();
+            }
+            else
+            {
+                Exit();
+            }
+        }
+
+        static void Register()
+        {
+            bool containsNumber = false;
+            bool correctLenght = false;
+            bool hasSpace;
+            string name = string.Empty;
+            string password = string.Empty;
+            
+            Console.WriteLine("REGISTRATION");    
+            do
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+
+                containsNumber = name.Any(c => char.IsDigit(c)); // check kung my number, mag rereturn ng bool
+                hasSpace = name.Contains(" "); // check kung my space, mag rereturn ng bool
+
+                if (containsNumber)
+                    Console.WriteLine("Your name has a number! "); // message lang does not validate anything.
+
+                if (hasSpace)
+                    Console.WriteLine("Your name has a space! ");
+
+                if (name == "")
+                    Console.WriteLine("Enter a name! ");
+            }
+            while (containsNumber || hasSpace || name == ""); // actual validation, mag loloop pag may nag true.
+
+            do
+            {
+                const int Min = 8;
+                const int Max = 16;
+
+                Console.Write("Enter your password: ");
+                password = Console.ReadLine();
+
+                int lenght = password.Length;
+
+                if (lenght < Min || lenght > Max)
+                {
+                    Console.WriteLine($"Password should be {Min} to {Max} characters. ");
+                    correctLenght = false;
+                }
+                else if(password.Trim() == "")
+                {
+                    Console.WriteLine($"Enter a password!");
+                }
+                else
+                {
+                    correctLenght = true;
+                }
+            }
+            while (!correctLenght || password.Trim() == ""); // actual validation, mag loloop pag may nag true.
+
+            StorePassword(name, password);
+            Console.WriteLine($"You are now registered.");
+
+            MainMenu();
+        }
+
+        public static void Login()
+        {
+            string name = string.Empty;
+            string password = string.Empty;
+
+            Console.WriteLine("LOGIN");
+            do
+            {
+
+                Console.Write("Enter name: ");
+                name = Console.ReadLine();
+                Console.Write("Enter password: ");
+                password = Console.ReadLine();
+            }
+            while (!ValidatePassword(name, password)); // icacall ung nasa baba with name and password as parameters
+
+            Console.WriteLine("You are now logged in!");
+            MainMenu();
+        }
+
+        static void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        // store password method
+        static void StorePassword(string username, string password)
+        {
+            string[] credentials = new string[] { username, password };
+            System.IO.File.WriteAllLines(@"C:\Users\Benjo\Desktop\test.txt", credentials); //change mo sa path ng desktop mo. auto create na ung notepad
+        }
+
+        // validate password method, will return a boolean
+        static bool ValidatePassword(string username, string password)
+        {
+            string[] content = System.IO.File.ReadAllLines(@"C:\Users\Benjo\Desktop\test.txt");  
+
+            if (username != content[0] && password != content[1])
+            {
+                Console.WriteLine("Wrong name or password.");
+                return false;
+            }
+            else
+                return true;
+        }
+
+    }
+}
---
+++ PracticeConsole/PracticeConsole/UseDelegate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PracticeConsole
+{
+    class UseDelegate
+    {
+        public delegate int Calculate(int x, int y);
+
+        public UseDelegate()
+        {
+            //Calculate calc = (x, y) => x + y;
+            //Console.Write(calc(5, 5));
+
+            //int[] value = { 1, 2, 3, 4, 5, 6 };
+
+            //int i = 0;
+            //while (i < value.Length)
+            //{
+            //    Console.WriteLine(value[i]);
+            //    i++;
+            //    Task.Delay(5000);
+
+            //}
+            //Console.ReadKey();
+
+            //int x = 0;
+            //do
+            //{
+            //    Console.WriteLine(value[x]);
+            //    x++;
+            //}
+            //while (x < value.Length);
+            //Console.ReadKey();
+
+        }
+           
+    }
+}
